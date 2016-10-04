48dc7bd2f53e22dde638ec8afc45ae31baeef468
Yana Slavcheva <yana.slavcheva@gmail.com>
2016-10-04 09:28:39 +0300

    Added favicon; Added site.css; altered url parse

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 let http = require('http')
 let fs = require('fs')
+let url = require('url')
+
+let portNumber = 1112
 
 http
     .createServer((req, res) => {
-      fs.readFile('./index.html', (err, data) => {
-        // TODO: manage the err as proper res
-        if (err) console.log(err)
-        res.writeHead(200)
-        res.write(data)
+      var parsedUrl = url.parse(req.url).pathname
+
+      if (parsedUrl === '/favicon.ico') {
+        fs.readFile('./favicon.ico', (err, data) => {
+          // TODO: manage the err as proper res
+          if (err) console.log(err)
+          res.writeHead(200)
+          res.write(data)
+          res.end()
+        })
+      } else if (parsedUrl === '/') {
+        fs.readFile('./index.html', (err, data) => {
+          // TODO: manage the err as proper res
+          if (err) console.log(err)
+          res.writeHead(200, {
+            'Content-Type': 'text/html'
+          })
+          res.write(data)
+          res.end()
+        })
+      } else {
+        res.writeHead(404)
+        res.write('404 Page Not Found')
         res.end()
-      })
+      }
     })
-    .listen(1234)
+    .listen(portNumber)
+
+console.log(`Server listening on port ${portNumber}`)
 
