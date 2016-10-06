b59c8be93fe4c86be7b58eaeeecbf7b5f67b38cd
Yana Slavcheva <yana.slavcheva@gmail.com>
2016-10-06 21:03:10 +0300

    Restrict the static file handler

--- FirstServerTest/handlers/read-file.js
+++ FirstServerTest/handlers/read-file.js
@@ -1,13 +1,32 @@
 let fs = require('fs')
 let url = require('url')
 
+function checkIfFileExtensionIsAllowed (req) {
+  let isFileExtensionAllowed = false
+  let allowedFileExtensions = [
+    '.css',
+    '.js',
+    '.html',
+    '.jpg'
+  ]
+
+  for (let fileExtension of allowedFileExtensions) {
+    if (req.pathname.endsWith(fileExtension)) {
+      isFileExtensionAllowed = true
+    }
+  }
+
+  return isFileExtensionAllowed
+}
+
 module.exports = function (req, res) {
   req.pathname = req.pathname || url.parse(req.url).pathname
 
   fs.readFile('.' + req.pathname, (err, data) => {
-        // TODO: fix security issue, pls ;)
-        // TODO: allow getting files only from content folder
-    if (err) {
+    let isFileFolderAllowed = req.pathname.startsWith('/content')
+    let isFileExtensionAllowed = checkIfFileExtensionIsAllowed(req)
+
+    if (err || !isFileFolderAllowed || !isFileExtensionAllowed) {
       res.writeHead(404)
       res.write('404 Page Not Found')
       res.end()
@@ -20,6 +39,10 @@ module.exports = function (req, res) {
       contentType = 'text/css'
     } else if (req.pathname.endsWith('.js')) {
       contentType = 'application/javascript'
+    } else if (req.pathname.endsWith('.jpg')) {
+      contentType = 'image/jpeg'
+    } else if (req.pathname.endsWith('.html')) {
+      contentType = 'text/html'
     }
 
     res.writeHead(200, {
