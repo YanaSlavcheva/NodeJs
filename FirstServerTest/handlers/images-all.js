689dfe9ee116c1e178fe4b8a6ded94295bef8a5a
Yana Slavcheva <yana.slavcheva@gmail.com>
2016-10-11 00:19:18 +0300

    images data from memory array loaded into all images view

--- FirstServerTest/handlers/images-all.js
+++ FirstServerTest/handlers/images-all.js
@@ -1,25 +1,21 @@
 let url = require('url')
-let mustache = require('./../node_modules/mustache/mustache')
 let fs = require('fs')
+
+let mustache = require('./../node_modules/mustache/mustache')
+
+let imagesInfo = require('./../my-modules/images-info-container.js')
 
 module.exports = function (req, res) {
   let continueWithNextHandler = false
   req.pathname = req.pathname || url.parse(req.url).pathname
 
-  var itemToDisplay = []
-  itemToDisplay.push({
-    path: './',
-    template: 'images-all.html',
-    view: {
-      name: 'test image name',
-      url: 'www.testimageurl'
-    }
-  })
+  var template = 'images-all.html'
 
-  var data = {images: [{name: 'Ryan Pays', url: 'http://www.ryanpays.com'}, {name: 'foo', url: 'http://www.google.com'}]}
+  // var data = {images: [{name: 'Ryan Pays', url: 'http://www.ryanpays.com'}, {name: 'foo', url: 'http://www.google.com'}]}
+  var data = {images: imagesInfo}
 
   if (req.pathname === '/images/all') {
-    fs.readFile(itemToDisplay[0].template, function (err, template) {
+    fs.readFile(template, function (err, template) {
       if (err) throw err
 
       res.writeHead(200, {
