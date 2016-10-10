fb9fc6e3c703566931400404e188b614174600b0
Yana Slavcheva <yana.slavcheva@gmail.com>
2016-10-10 23:50:09 +0300

    Added mustache all-images template

--- FirstServerTest/handlers/images-all.js
+++ FirstServerTest/handlers/images-all.js
@@ -1,11 +1,32 @@
 let url = require('url')
+let mustache = require('./../node_modules/mustache/mustache')
+let fs = require('fs')
 
 module.exports = function (req, res) {
   let continueWithNextHandler = false
   req.pathname = req.pathname || url.parse(req.url).pathname
 
+  var itemToDisplay = []
+  itemToDisplay.push({
+    path: './',
+    template: 'images-all.html',
+    view: {
+      name: 'test image name',
+      url: 'www.testimageurl'
+    }
+  })
+
   if (req.pathname === '/images/all') {
-    // https://weekendlessons.wordpress.com/2011/04/18/node-js-with-mustache-get-started/
+    fs.readFile(itemToDisplay[0].template, function (err, template) {
+      if (err) throw err
+
+      res.writeHead(200, {
+        'Content-Type': 'text/html'
+      })
+      template = template.toString()
+      res.write(mustache.to_html(template, itemToDisplay[0].view))
+      res.end()
+    })
   } else {
     continueWithNextHandler = true
   }
--- FirstServerTest/handlers/index.js
+++ FirstServerTest/handlers/index.js
@@ -2,10 +2,12 @@ let favicon = require('./favicon')
 let homePage = require('./home-page')
 let addImageInfo = require('./add-image-info')
 let readFile = require('./read-file')
+let imagesAll = require('./images-all')
 
 module.exports = [
   favicon,
   homePage,
   addImageInfo,
+  imagesAll,
   readFile
 ]
